41367804c72ef222606de0906037be3255d3b76a
Jeff Harris <jeffharris@openai.com>
2023-04-30 10:45:49 -0700

    Improve styling + layout

--- options.js
+++ options.js
@@ -24,10 +24,28 @@ function updateModelList() {
 
     models.forEach((model, index) => {
       const listItem = document.createElement('li');
+      listItem.style.display = 'flex';
+      listItem.style.justifyContent = 'space-between';
+      listItem.style.alignItems = 'center';
+      listItem.style.marginBottom = '16px';
 
-      const modelInfo = document.createElement('span');
-      modelInfo.textContent = `${model.displayName} - ${model.url}`;
-      listItem.appendChild(modelInfo);
+      const displayNameWrapper = document.createElement('div');
+      displayNameWrapper.style.width = 'calc(33% - 8px)';
+
+      const displayName = document.createElement('span');
+      displayName.textContent = model.displayName;
+      displayNameWrapper.appendChild(displayName);
+      listItem.appendChild(displayNameWrapper);
+
+      const modelUrlWrapper = document.createElement('div');
+      modelUrlWrapper.style.width = 'calc(33% - 8px)';
+      modelUrlWrapper.style.textAlign = 'center';
+
+      const modelUrl = document.createElement('span');
+      modelUrl.textContent = model.url;
+      modelUrlWrapper.appendChild(modelUrl);
+
+      listItem.appendChild(modelUrlWrapper);
 
       const deleteIcon = document.createElement('span');
       deleteIcon.innerHTML = '<svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>';
@@ -39,6 +57,7 @@ function updateModelList() {
     });
   });
 }
+
 
 updateModelList();
 
